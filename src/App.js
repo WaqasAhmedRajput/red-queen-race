2c7ce3c1d135044415176b671c1e80a9337d962e
Waqas Ahmed <waqasrajput35@yahoo.com>
2020-07-20 12:25:31 +0500

    Start Testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Bg from './images/bush_small.png'
 function App() {
   return (
     <div className="App">
+      <img src={Bg} />
       <h1>Welcome</h1>
     </div>
   );

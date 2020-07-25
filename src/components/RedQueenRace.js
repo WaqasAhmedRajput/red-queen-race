2fdb68d1be57e10e6cab5fe211c4c455f3be7390
Waqas Ahmed <waqasrajput35@yahoo.com>
2020-07-25 16:15:17 +0500

    All Project are done

--- src/components/RedQueenRace.js
+++ src/components/RedQueenRace.js
@@ -1,10 +1,172 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useWebAnimations from '@wellyshen/use-web-animations';
 import './style.css';
 
+
 export const RedQueenRace = () => {
+    var playbackrateRQ = 1;
+    var playbackrateBG = 0;
+
+    const sceneryFrames = [
+        { transform: "translateX(100%)" },
+        { transform: "translateX(-100%)" },
+    ];
+    const sceneryTimingBackground = {
+        duration: 36000,
+        iterations: Infinity,
+        playbackRate: playbackrateBG,
+    };
+
+    const sceneryTimingForeground = {
+        duration: 12000,
+        iterations: Infinity,
+        playbackRate: playbackrateBG,
+    };
+    const background1Movement = useWebAnimations({
+        keyframes: sceneryFrames,
+        timing: sceneryTimingBackground,
+    });
+    const background2Movement = useWebAnimations({
+        keyframes: sceneryFrames,
+        timing: sceneryTimingBackground,
+    });
+    const foreground1Movement = useWebAnimations({
+        keyframes: sceneryFrames,
+        timing: sceneryTimingForeground,
+    });
+    const foreground2Movement = useWebAnimations({
+        keyframes: sceneryFrames,
+        timing: sceneryTimingForeground,
+    });
+    const spriteFrames = [
+        { transform: "translateY(0)" },
+        { transform: "translateY(-100%)" },
+    ];
+    const spirteTiming = {
+        easing: "steps(7, end)",
+        direction: "reverse",
+        duration: 600,
+        playbackRate: playbackrateRQ,
+        iterations: Infinity,
+    };
+    const redQueen_alice = useWebAnimations({
+        keyframes: spriteFrames,
+        timing: spirteTiming,
+    });
+    const adjustBackgroundPlayback = () => {
+        if (playbackrateRQ < 0.8) {
+            playbackrateBG = (playbackrateRQ / 2) * -1;
+        } else if (playbackrateRQ > 1.2) {
+            playbackrateBG = playbackrateRQ / 2;
+        } else {
+            playbackrateBG = 0;
+        }
+        foreground1Movement.getAnimation().playbackRate = playbackrateBG;
+        foreground2Movement.getAnimation().playbackRate = playbackrateBG;
+        background1Movement.getAnimation().playbackRate = playbackrateBG;
+        background2Movement.getAnimation().playbackRate = playbackrateBG;
+    };
+    useEffect(() => {
+        const fganimation = foreground1Movement.getAnimation();
+        fganimation.currentTime = fganimation.effect.getTiming().duration / 2;
+
+        const bganimation = background1Movement.getAnimation();
+        bganimation.currentTime = bganimation.effect.getTiming().duration / 2;
+
+        setInterval(() => {
+            if (playbackrateRQ > 0.4) {
+                playbackrateRQ *= 0.9;
+                redQueen_alice.getAnimation().playbackRate = playbackrateRQ;
+            }
+            adjustBackgroundPlayback();
+        }, 3000);
+
+        document.addEventListener("click", () => {
+            playbackrateRQ *= 1.1;
+            redQueen_alice.getAnimation().playbackRate = playbackrateRQ;
+            adjustBackgroundPlayback();
+        });
+    });
     return (
         <div>
+            <div className="wrapper">
+                <div className="sky"></div>
+                <div className="earth">
+                    <div id="red-queen_and_alice">
+                        <img
+                            id="red-queen_and_alice_sprite"
+                            src="./images/sprite_running-alice-queen_small.png"
+                            srcSet="./images/sprite_running-alice-queen.png 2x"
+                            alt="Alice and the Red Queen running to stay in place."
+                            ref={redQueen_alice.ref}
+                        />
+                    </div>
+                </div>
+
+                <div className="scenery" id="foreground1" ref={foreground1Movement.ref}>
+                    <img
+                        id="palm3"
+                        src="./images/palm3_small.png"
+                        srcSet="./images/palm3.png 2x"
+                        alt=" "
+                    />
+                </div>
+                <div className="scenery" id="foreground2" ref={foreground2Movement.ref}>
+                    <img
+                        id="bush"
+                        src="./images/bush_small.png"
+                        srcSet="./images/bush.png 2x"
+                        alt=" "
+                    />
+                    <img
+                        id="w_rook_upright"
+                        src="./images/w_rook_upright_small.png"
+                        srcSet="./images/w_rook_upright.png 2x"
+                        alt=" "
+                    />
+                </div>
+                <div className="scenery" id="background1" ref={background1Movement.ref}>
+                    <img
+                        id="r_pawn_upright"
+                        src="./images/r_pawn_upright_small.png"
+                        srcSet="./images/r_pawn_upright.png 2x"
+                        alt=" "
+                    />
+                    <img
+                        id="w_rook"
+                        src="./images/w_rook_small.png"
+                        srcSet="./images/w_rook.png 2x"
+                        alt=" "
+                    />
+                    <img
+                        id="palm1"
+                        src="./images/palm1_small.png"
+                        srcSet="./images/palm1.png 2x"
+                        alt=" "
+                    />
+                </div>
+                <div className="scenery" id="background2" ref={background2Movement.ref}>
+                    <img
+                        id="r_pawn"
+                        src="./images/r_pawn_small.png"
+                        srcSet="./images/r_pawn.png 2x"
+                        alt=" "
+                    />
+
+                    <img
+                        id="r_knight"
+                        src="./images/r_knight_small.png"
+                        srcSet="./images/r_knight.png 2x"
+                        alt=" "
+                    />
+                    <img
+                        id="palm2"
+                        src="./images/palm2_small.png"
+                        srcSet="./images/palm2.png 2x"
+                        alt=" "
+                    />
+                </div>
+            </div>
 
         </div>
     );
